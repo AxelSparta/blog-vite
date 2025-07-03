3bc05b64db7099fef32c4196f95b3750accdda0e
AxelSparta <axelnicolassparta@gmail.com>
2025-07-03 20:55:15 -0300

    chore: update config file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const isProd = import.meta.env.PROD
 
 export const serverUrl = isProd
-  ? import.meta.env.VITE_SERVER_PROD
+  ? import.meta.env.VITE_SERVER_URL
   : import.meta.env.VITE_SERVER_LOCAL
